453091a05eded4fd680d62a4b8fc21ad5d80ca64
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-04-01 14:08:33 -0600

    fixed port to get fastapi application working

--- Backend/detection_api.py
+++ Backend/detection_api.py
@@ -546,5 +546,5 @@ async def test_alert(background_tasks: BackgroundTasks):
 if __name__ == "__main__":
     import uvicorn
     logger.info("Starting API server")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
    
